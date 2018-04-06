1377d5f0a346a75ecf9209525ef6fea44bf7d33d
Roald Hacquebord <roald@hacquebordbuckles.nl>
2018-04-06 23:58:23 +0200

    Add view for civilians with voting form

--- repitup-frontend/src/app/app.module.ts
+++ repitup-frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import * as data from './config/tokenabis.json';
@@ -9,11 +10,16 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { MunicipalityComponent } from './municipality/municipality.component';
 import { CivilComponent } from './civil/civil.component';
+import { CivilVoteFormComponent } from './civil/civil-vote-form.component';
 import { EntrepreneurComponent } from './entrepreneur/entrepreneur.component';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
 
 import { KvkIdentityWidgetComponent } from './kvk-identity-widget/kvk-identity-widget.component';
 
@@ -23,14 +29,20 @@ import { KvkIdentityWidgetComponent } from './kvk-identity-widget/kvk-identity-w
     MainComponent,
     MunicipalityComponent,
     CivilComponent,
+    CivilVoteFormComponent,
     EntrepreneurComponent,
     KvkIdentityWidgetComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
     MatGridListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
