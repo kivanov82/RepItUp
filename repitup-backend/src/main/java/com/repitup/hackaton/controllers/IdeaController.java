9f29594f091ba9118d129221ff82b898007cb3f3
Alex B <>
2018-04-06 21:08:08 +0200

    Idea object added, id moved to parent class

---
+++ repitup-backend/src/main/java/com/repitup/hackaton/controllers/IdeaController.java
@@ -0,0 +1,43 @@
+package com.repitup.hackaton.controllers;
+
+import com.repitup.hackaton.model.Idea;
+import com.repitup.hackaton.model.Transaction;
+import com.repitup.hackaton.repository.IdeaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 06-Apr-18
+ * Time: 19:44
+ */
+@RestController
+@RequestMapping("idea")
+public class IdeaController {
+
+    @Autowired
+    private IdeaRepository ideaRepository;
+
+
+    @RequestMapping(value = "/get.json", method = RequestMethod.GET)
+    public Idea getIdea(Idea idea) {
+        return idea;
+    }
+
+    @RequestMapping(value = "/getAll.json", method = RequestMethod.GET)
+    public List<Idea> findAll() {
+        return (List<Idea>) ideaRepository.findAll();
+    }
+
+    @RequestMapping(value = "/save.json", method = RequestMethod.POST)
+    public void saveIdea(String description) {
+        Idea idea = new Idea(description);
+        ideaRepository.save(idea);
+    }
+
+
+}
--- repitup-backend/src/test/java/com/repitup/hackaton/repository/TransactionRepositoryTest.java
+++ repitup-backend/src/test/java/com/repitup/hackaton/repository/TransactionRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.repitup.hackaton.repository;
 
 import com.repitup.hackaton.model.Transaction;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
