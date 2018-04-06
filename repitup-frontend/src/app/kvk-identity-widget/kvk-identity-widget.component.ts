70bbbf5344eca86fc7287bc1e31a6f2625aa5c60
Roald Hacquebord <roald@hacquebordbuckles.nl>
2018-04-06 22:08:58 +0200

    Handle kvk id widget events

--- repitup-frontend/src/app/kvk-identity-widget/kvk-identity-widget.component.ts
+++ repitup-frontend/src/app/kvk-identity-widget/kvk-identity-widget.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { KvkIdentityService } from '../kvk-identity/kvk-identity.service';
 import { KvkIdentityServiceProvider } from '../kvk-identity/kvk-identity.service.provider';
 
@@ -10,11 +10,23 @@ import { KvkIdentityServiceProvider } from '../kvk-identity/kvk-identity.service
 export class KvkIdentityWidgetComponent implements AfterViewInit {
 
   @Input() questionId: string;
+  @Output() onAgree = new EventEmitter();
+  @Output() onReject = new EventEmitter();
+  @Output() onError = new EventEmitter();
+
+  @ViewChild('container') container;
   @ViewChild('widget') widget;
 
   constructor(private identityService: KvkIdentityService) {}
 
   ngAfterViewInit() {
-    this.identityService.createWidget(this.widget, this.questionId);
+    this.identityService.createWidget(this.container, this.questionId)
+      .then(() => {
+        this.widget.nativeElement.onAgree = (sessionKey, data) => {
+          this.onAgree.emit(sessionKey, data);
+        };
+        this.widget.nativeElement.onReject = () => { this.onReject.emit() };
+        this.widget.nativeElement.onError = () => { this.onError.emit() };
+      });
   }
 }
