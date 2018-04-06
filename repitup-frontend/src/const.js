cb0d29886b86626e24b4f354cdbc31d6396beea1
kirilivanov <k.ivanov@loyalgarden.co>
2018-04-06 16:58:55 +0200

    web3 support

---
+++ repitup-frontend/src/const.js
@@ -0,0 +1,9 @@
+const tokenAddress = '0x4e71920b7330515faf5ea0c690f1ad06a85fb60c';
+const vaultStabilityAddress = '0xde5491f774f0cb009abcea7326342e105dbb1b2e';
+const vaultProjectAddress = '0xd54b47f8e6a1b97f3a84f63c867286272b273b7c';
+
+
+const owner = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
+const recipient = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+const event = '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef';
+
