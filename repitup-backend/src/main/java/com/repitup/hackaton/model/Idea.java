3993cf6d9ddeae8a39acd37fd56d37d1c8c59887
Alex B <>
2018-04-07 17:31:37 +0200

    imports removed

--- repitup-backend/src/main/java/com/repitup/hackaton/model/Idea.java
+++ repitup-backend/src/main/java/com/repitup/hackaton/model/Idea.java
@@ -1,11 +1,8 @@
 package com.repitup.hackaton.model;
 
-import javafx.scene.chart.PieChart;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
