535621931abfe25ad77e0b8a7e88db54b320321b
kirilivanov <k.ivanov@loyalgarden.co>
2018-04-07 15:21:08 +0200

    Smart contracts

--- repitup-backend/contracts/migrations/3_rating.js
+++
@@ -1,5 +0,0 @@
-var Rating = artifacts.require("./Rating.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(Rating, ['Bus1', 'Bus2']);
-};
--- repitup-frontend/src/const.js
+++ repitup-frontend/src/const.js
@@ -1,6 +1,6 @@
-const tokenAddress = '0x8065f4c7b8c2bf53561af92d9da2ea022a0b28ca';
-const votingTokenAddress = '0x4a5cd58b24e3bf04360b06bfeaf45a39aa8035b6';
-const ratingAddress = '0xcb152a2aa90055a0d255ca7dbaeb85edfdc86096';
+const tokenAddress = '0xace937991395b2051613368206066eb2e752a333';
+const votingTokenAddress = '0x7bf7ae2da6013aa8de29627e29e4b9fa807d4469';
+const reputationAddress = '0xe53a3bac98c9d304a3bb6ce34ed37923bc25117a';
 
 
 const municipality = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57';
