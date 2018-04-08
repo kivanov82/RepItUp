b25db848a001aa668437944873484848b8e9af92
Roald Hacquebord <roald@hacquebordbuckles.nl>
2018-04-08 09:47:34 +0200

    Update challenge content

--- repitup-frontend/src/app/challenges.ts
+++ repitup-frontend/src/app/challenges.ts
@@ -2,23 +2,23 @@ import { Challenge } from './challenge';
 
 export const CHALLENGES: Challenge[] = [
   {
-    title: "Lorem ipsum dolor amet",
+    title: "Vacancy challenge",
     subtitle: 'Voorstreek 55, Leeuwarden',
     squareMeters: "300",
     image: "assets/challenge-placeholder-3.png",
     by: "Bruisende Binnenstad"
   },
   {
-    title: "In sint paleo",
-    subtitle: 'Hoendiep 57, Groningen',
-    squareMeters: "27",
+    title: "Vacancy challenge",
+    subtitle: 'Grote Kerkstraat 69, Leeuwarden',
+    squareMeters: "120",
     image: "assets/challenge-placeholder-1.jpg",
     by: "Nick Muller"
   },
   {
-    title: "Tote bag labore pork ",
-    subtitle: 'Sparkstraats 20, Groningen',
-    squareMeters: "12",
+    title: "Vacancy challenge",
+    subtitle: 'Nieuwestad 23, Leeuwarden',
+    squareMeters: "170",
     image: "assets/challenge-placeholder-2.jpg",
     by: "Dada Petja"
   }
