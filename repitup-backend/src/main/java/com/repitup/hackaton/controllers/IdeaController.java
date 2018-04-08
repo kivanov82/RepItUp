5a28c60d84cd7fa1deba77ab3f46f841001b4cac
kirilivanov <k.ivanov@loyalgarden.co>
2018-04-08 09:22:22 +0200

    Get last

--- repitup-backend/src/main/java/com/repitup/hackaton/controllers/IdeaController.java
+++ repitup-backend/src/main/java/com/repitup/hackaton/controllers/IdeaController.java
@@ -1,12 +1,15 @@
 package com.repitup.hackaton.controllers;
 
+import java.util.List;
+
 import com.repitup.hackaton.model.Idea;
-import com.repitup.hackaton.model.Transaction;
 import com.repitup.hackaton.repository.IdeaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,6 +27,12 @@ public class IdeaController {
     @RequestMapping(value = "/{idea}", method = RequestMethod.GET)
     public Idea getIdea(@PathVariable Idea idea) {
         return idea;
+    }
+
+    @RequestMapping(value = "/getLast", method = RequestMethod.GET)
+    public Idea findLast() {
+        List<Idea> ideas = (List<Idea>)ideaRepository.findAll();
+        return ideas.get(ideas.size() - 1);
     }
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
