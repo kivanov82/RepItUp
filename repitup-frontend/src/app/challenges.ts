c7e371bfd667d94093fb727a82b23e85470c9cf0
Roald Hacquebord <roald@hacquebordbuckles.nl>
2018-04-08 09:34:24 +0200

    Update challenge placeholder images

--- repitup-frontend/src/app/challenges.ts
+++ repitup-frontend/src/app/challenges.ts
@@ -5,21 +5,21 @@ export const CHALLENGES: Challenge[] = [
     title: "Lorem ipsum dolor amet",
     subtitle: 'Hoestraat 10, Den Haag',
     squareMeters: "300",
-    image: "assets/challenge-placeholder.png",
+    image: "assets/challenge-placeholder-3.png",
     by: "Nick Brides"
   },
   {
     title: "In sint paleo",
     subtitle: 'Hoendiep 57, Groningen',
     squareMeters: "27",
-    image: "assets/challenge-placeholder.png",
+    image: "assets/challenge-placeholder-1.jpg",
     by: "Nick Muller"
   },
   {
     title: "Tote bag labore pork ",
     subtitle: 'Sparkstraats 20, Groningen',
     squareMeters: "12",
-    image: "assets/challenge-placeholder.png",
+    image: "assets/challenge-placeholder-2.jpg",
     by: "Dada Petja"
   }
 ];
