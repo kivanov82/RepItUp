1867beb58beac38a02e1845a1b6e9948ecb5d25a
Roald Hacquebord <roald@hacquebordbuckles.nl>
2018-04-08 11:31:53 +0200

    Move challenges to json for code score

--- repitup-frontend/src/app/challenges.ts
+++ repitup-frontend/src/app/challenges.ts
@@ -1,36 +1,6 @@
 import { Challenge } from './challenge';
+import * as challengeSeed from './challengeSeed.json';
 
-export const CHALLENGES: Challenge[] = [
-  {
-    title: "Vacancy challenge",
-    subtitle: 'Voorstreek 55, Leeuwarden',
-    squareMeters: "300",
-    image: "assets/challenge-placeholder-3.png",
-    by: "Bruisende Binnenstad",
-    description: `This authentic building is vacant for over
-a year and is looking for an
-entrepreneur who will bring it back to life.
-Do you think your idea suits with
-the property and match well
-with the local community?
-Submit your idea and see
-how feasible it is by checking
-with experts and the local community.`
-  },
-  {
-    title: "Vacancy challenge",
-    subtitle: 'Grote Kerkstraat 69, Leeuwarden',
-    squareMeters: "120",
-    image: "assets/challenge-placeholder-1.jpg",
-    by: "Bruisende Binnenstad",
-    description: "Lorem ipsum dolor amet selvage messenger bag twee woke salvia man braid banjo. Typewriter gluten-free live-edge, paleo vinyl kickstarter humblebrag cronut leggings cornhole. Ramps hoodie seitan gochujang letterpress blog you probably haven't heard of them tbh shaman four dollar toast adaptogen live-edge literally. Meditation +1 chillwave, viral asymmetrical bushwick hoodie. Tousled synth whatever mixtape tattooed chia."
-  },
-  {
-    title: "Vacancy challenge",
-    subtitle: 'Nieuwestad 23, Leeuwarden',
-    squareMeters: "170",
-    image: "assets/challenge-placeholder-2.jpg",
-    by: "Municipality Leeuwarden",
-    description: "Lorem ipsum dolor amet selvage messenger bag twee woke salvia man braid banjo. Typewriter gluten-free live-edge, paleo vinyl kickstarter humblebrag cronut leggings cornhole. Ramps hoodie seitan gochujang letterpress blog you probably haven't heard of them tbh shaman four dollar toast adaptogen live-edge literally. Meditation +1 chillwave, viral asymmetrical bushwick hoodie. Tousled synth whatever mixtape tattooed chia."
-  }
-];
+export const CHALLENGES: Challenge[] = [];
+
+challengeSeed.forEach(challenge => CHALLENGES.push(challenge));
